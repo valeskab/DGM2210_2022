d86fcf4de99b91ed83223abe8bd331c1b2ce9d7b
Valeska <54381257+valeskab@users.noreply.github.com>
2023-09-12 19:44:45 -0600

    Update BrokenFK.py

--- PythonScripts/BrokenFK.py
+++ PythonScripts/BrokenFK.py
@@ -10,11 +10,13 @@ p_constraint1 = cmds.parentConstraint(parent_ctrl, child_ctrl_grp, mo=True, skip
 p_constraint2 = cmds.parentConstraint(parent_ctrl, child_ctrl_grp, mo=True, skipTranslate=['x','y','z'], weight=1)[0]
 cmds.scaleConstraint(parent_ctrl, child_ctrl_grp, weight=1)
 
-cmds.addAttr(child_ctrl, ln='FollowTranslate', at='double', min=0, max=1, dv=1)
-cmds.setAttr('%s.FollowTranslate' % (child_ctrl), e=True, keyable=True)
+if not cmds.attributeQuery('FollowTranslate', node=child_ctrl, exists=True):
+  cmds.addAttr(child_ctrl, ln='FollowTranslate', at='double', min=0, max=1, dv=1)
+  cmds.setAttr('%s.FollowTranslate' % (child_ctrl), e=True, keyable=True)
 
-cmds.addAttr(child_ctrl, ln='FollowRotate', at='double', min=0, max=1, dv=1)
-cmds.setAttr('%s.FollowRotate' % (child_ctrl), e=True, keyable=True)
+if not cmds.attributeQuery('FollowRotate', node=child_ctrl, exists=True):
+  cmds.addAttr(child_ctrl, ln='FollowRotate', at='double', min=0, max=1, dv=1)
+  cmds.setAttr('%s.FollowRotate' % (child_ctrl), e=True, keyable=True)
 
 cmds.connectAttr('%s.FollowTranslate' % (child_ctrl), '%s.w0' % (p_constraint1), f=True)
 cmds.connectAttr('%s.FollowRotate' % (child_ctrl), '%s.w0' % (p_constraint2), f=True)
