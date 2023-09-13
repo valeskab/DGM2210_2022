e8fedef48cc7a0a1900d34c1c97eb013cd6f2425
Valeska <54381257+valeskab@users.noreply.github.com>
2023-09-12 19:41:53 -0600

    Update BrokenFK.py

--- PythonScripts/BrokenFK.py
+++ PythonScripts/BrokenFK.py
@@ -6,9 +6,9 @@ child_ctrl = sels[1]
 
 child_ctrl_grp = cmds.listRelatives(child_ctrl, parent=True)[0]
 
-p_constraint1 = cmds.parentConstraint(mo=True, skipRotate=['x','y','z'], weight=1)[0]
-
-p_constraint2 = cmds.parentConstraint(mo=True, skipTranslate=['x','y','z'], weight=1)[0]
+p_constraint1 = cmds.parentConstraint(parent_ctrl, child_ctrl_grp, mo=True, skipRotate=['x','y','z'], weight=1)[0]
+p_constraint2 = cmds.parentConstraint(parent_ctrl, child_ctrl_grp, mo=True, skipTranslate=['x','y','z'], weight=1)[0]
+cmds.scaleConstraint(parent_ctrl, child_ctrl_grp, weight=1)
 
 cmds.addAttr(child_ctrl, ln='FollowTranslate', at='double', min=0, max=1, dv=1)
 cmds.setAttr('%s.FollowTranslate' % (child_ctrl), e=True, keyable=True)
